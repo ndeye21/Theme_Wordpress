16fa27879ee50d2e2b65e3265f2a51b748b6ae0d
ndeye21 <ndeyelgi@gmail.com>
2018-12-17 14:14:11 +0000

    first commit

---
+++ footer.php
@@ -0,0 +1,6 @@
+<footer>
+      <p>Copyright &copy; 2017</p>
+    </footer>
+    <?php wp_footer(); ?>
+  </body>
+</html>
---
+++ functions.php
@@ -0,0 +1,30 @@
+<?php
+// This function enqueues the Normalize.css for use. The first parameter is a name for the stylesheet, the second is the URL. Here we
+// use an online version of the css file.
+function add_normalize_CSS() {
+    wp_enqueue_style( 'normalize-styles', "https://cdnjs.cloudflare.com/ajax/libs/normalize/7.0.0/normalize.min.css");
+}
+
+// Register a new sidebar simply named 'sidebar'
+function add_widget_Support() {
+    register_sidebar( array(
+                    'name'          => 'Sidebar',
+                    'id'            => 'sidebar',
+                    'before_widget' => '<div>',
+                    'after_widget'  => '</div>',
+                    'before_title'  => '<h2>',
+                    'after_title'   => '</h2>',
+    ) );
+}
+// Hook the widget initiation and run our function
+add_action( 'widgets_init', 'add_Widget_Support' );
+
+// Register a new navigation menu
+function add_Main_Nav() {
+    register_nav_menu('header-menu',__( 'Header Menu' ));
+  }
+  // Hook to the init action hook, run our navigation menu function
+  add_action( 'init', 'add_Main_Nav' );
+
+
+ 
